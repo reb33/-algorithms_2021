c7510d9a386fcdd7c6981ed4149257f83f3f5a21
k.barylnikov <ipt_git_bot@ipt.ru>
2021-05-07 12:52:21 +0300

    task4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,7 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -19,6 +20,19 @@ def func_1():
     m = 0
     num = 0
     for i in array:
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
+def func_3():
+    m = 0
+    num = 0
+    count_els = set(array)
+    for i in count_els:
         count = array.count(i)
         if count > m:
             m = count
@@ -41,3 +55,17 @@ def func_2():
 
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(timeit('func_1()', 'from __main__ import func_1'))
+print(timeit('func_2()', 'from __main__ import func_2'))
+print(timeit('func_3()', 'from __main__ import func_3'))
+
+
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+# 1.8374580730160233
+# 2.687536692013964 - медленей 1 алгоритма, т.к. есть 2 прохода по циклу (создание цикла new_array и max)
+# 1.4838648090080824 - быстрей 1 алгоритма, т.к. не вычисляется количество элементов повторно
+#                      для всех повторяющихся элементов
--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -17,6 +17,8 @@
 Укажите формулу сложности О-нотация каждого алгоритма
 и сделайте обоснование результатам.
 """
+from math import ceil, sqrt
+from timeit import timeit
 
 
 def simple(i):
@@ -39,5 +41,44 @@ def simple(i):
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+def sieve_of_eratosphenes(num):
+    coef = 10 if num < 6000 else 15
+    upper_board = num * coef
+    els = {i: True for i in range(1, upper_board)}
+    for i in range(2, ceil(sqrt(upper_board))):
+        if els[i]:
+            j = i * i
+            while j < upper_board:
+                els[j] = False
+                j += i
+    simples = [k for k, v in els.items() if v]
+    return simples[num]
+
+
+# rnd = int(input('Введите порядковый номер искомого простого числа:\n'))
+# print(simple(rnd))
+# print(sieve_of_eratosphenes(rnd))
+
+
+print('10 простое число метод перебора ', timeit('simple(10)', 'from __main__ import simple', number=1))
+print('10 простое число решето ', timeit('sieve_of_eratosphenes(10)', 'from __main__ import sieve_of_eratosphenes',
+                                         number=1))
+
+print('100 простое число метод перебора ', timeit('simple(100)', 'from __main__ import simple', number=1))
+print('100 простое число решето ',
+      timeit('sieve_of_eratosphenes(100)', 'from __main__ import sieve_of_eratosphenes', number=1))
+
+print('1000 простое число метод перебора ', timeit('simple(1000)', 'from __main__ import simple', number=1))
+print('1000 простое число решето ',
+      timeit('sieve_of_eratosphenes(1000)', 'from __main__ import sieve_of_eratosphenes', number=1))
+
+# 10 простое число метод перебора       3.358698450028896e-05
+# 10 простое число решето               0.000112134002847597
+# 100 простое число метод перебора      0.003166925016557798
+# 100 простое число решето              0.0003718429943546653
+# 1000 простое число метод перебора     0.38552172199706547
+# 1000 простое число решето             0.003140006010653451
+
+# при малых числах метод перебора быстрей
+# но уже при поиске 100 простого числа решето работает быстрей
+# при поиске 1000 простого числа решето работает в 100 раз быстрей
