13afa370c2c0bb3f4a5b90ad0641d62f73d38942
k.barylnikov <ipt_git_bot@ipt.ru>
2021-05-27 15:17:54 +0300

    start task8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,47 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter, namedtuple
+
+
+class Element:
+    __slots__ = ['value', 'num']
+
+    def __init__(self, value, num) -> None:
+        self.value = value
+        self.num = num
+
+
+class ListEls:
+
+    def __init__(self, counted_vals: dict) -> None:
+        self.els = [Element(*el) for el in sorted(counted_vals.items(), key=lambda x: x[1])]
+
+
+    def add(self, element=None):
+        for el in enumerate(self.els):
+            if element.num <= el.num:
+
+
+
+
+class ElTree:
+
+    def __init__(self, left, right) -> None:
+        self.left = left
+        self.right = right
+
+    def set_left(self, left):
+        self.left = left
+
+    def set_right(self, right):
+        self.right = right
+
+
+def main(text):
+    ListEls(Counter(text))
+    print()
+
+
+if __name__ == '__main__':
+    main('beep boop beer!')
